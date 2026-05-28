82ada23ffdafe6eff5a965d7aca70899dc62ca51
Andy Zhang <imapanda2009@gmail.com>
2026-05-28 13:53:44 -0400

    tmp

--- src/check.java
+++ src/check.java
@@ -3,7 +3,8 @@ public class check {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello worls");
+		System.out.println("hello world");
+		
 	}
 
 }
