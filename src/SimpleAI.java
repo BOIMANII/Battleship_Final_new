44d2fd71f7c4a5a4e5b507425ba4a7d0d268a691
unknown <anthonygordinario@gmail.com>
2026-05-28 13:56:14 -0400

    first new commit new repo

--- src/SimpleAI.java
+++ src/SimpleAI.java
@@ -3,7 +3,7 @@
 import java.util.Random;
 
 public class SimpleAI {
-
+//hi
 	public static int[] Guess() {
 		Random Rng = new Random();
 		int[] Guess = { Rng.nextInt(0, 11), Rng.nextInt(0, 11) };
